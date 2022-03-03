b207ea57c6a89f16c53952cb573c943f73685f5c
Adrien Vermersch <adrien.vermersch@epitech.eu>
2022-03-03 20:12:03 +0100

    resolving crash

--- src/scene/scene_menu.c
+++ src/scene/scene_menu.c
@@ -12,8 +12,8 @@ void scene_menu_call(game_t *game_manager)
     init_menu_buttons(game_manager);
     while (sfRenderWindow_isOpen(game_manager->window)) {
         analyse_events(game_manager);
+        sfRenderWindow_clear(game_manager->window, sfBlack);
         diplay_menu_buttons(game_manager);
-        sfRenderWindow_clear(game_manager->window, sfRed);
         sfRenderWindow_display(game_manager->window);
     }
 }
