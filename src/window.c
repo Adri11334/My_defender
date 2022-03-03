98bbefd7acef5e05d02b73357b1e04441a2a6777
Eleonore Bonaventure <eleonore.bonaventure@epitech.eu>
2022-03-03 20:08:10 +0100

    [ADD] | start button : play, settings, quit, how_to

--- src/window.c
+++ src/window.c
@@ -18,7 +18,7 @@ game_t *init_game(void)
 
     if (game_manager == NULL)
         return NULL;
-    game_manager->status = GAME;
+    game_manager->status = MENU;
     game_manager->window = sfRenderWindow_create(mode, "OUR defender !", \
     sfFullscreen, NULL);
     sfRenderWindow_setFramerateLimit(game_manager->window, 60);
