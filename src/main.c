2fa0c031ca7dc40d5e5f086c1c4f4be430ef4289
Adrien Vermersch <adrien.vermersch@epitech.eu>
2022-03-27 16:48:08 +0200

    adding -h template

--- src/main.c
+++ src/main.c
@@ -8,9 +8,23 @@
 #include <stddef.h>
 #include "my_defender.h"
 
+int display_help(void)
+{
+    char *text = NULL;
+
+    text = get_file_content("../assets/help.txt");
+    if (text != NULL)
+        write(1, text, my_strlen(text));
+    else
+        return 84;
+    return 0;
+}
+
 int main(int argc, char **argv, char **env)
 {
     if (*env == NULL)
         return 84;
+    if (my_strcmp(argv[1], "-h") == 0)
+        return display_help();
     return main_window(argv);
 }
